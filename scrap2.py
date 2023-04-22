fad2ea115cabda9e2e68c4ca3bf9dc2d1dbcecf1
RchardDayz <josedco@hotmail.com>
2023-04-22 09:59:20 -0600

    codigo actualizado

---
+++ scrap2.py
@@ -0,0 +1,128 @@
+#IMPORTANDO LIBRERIAS
+from csv import DictWriter
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+import os
+import random
+from datetime import date
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.common.exceptions import NoSuchElementException
+
+
+#LIMPIANDO PANTALLA
+os.system("cls")
+#os.system("del occtest.csv")
+
+# DEFINIENDO OPCIONES DE NAVEGADOR
+options = webdriver.FirefoxOptions()
+
+#Esta instruccione s para que minimice la ventana del explorador
+# options.add_argument('--headless')
+options.add_argument('--disable-extensions')
+options.add_argument('--disable-gpu')
+options.add_argument('--no-sandbox')
+options.binary_location = r'C:\Program Files\Mozilla Firefox\firefox.exe'
+
+# CREAR NAVEGADOR ROBOT ABRIENDO FIREFOX
+driver = webdriver.Firefox(executable_path="geckodriver", options=options)
+driver.delete_all_cookies()
+
+#Se establece la url
+url = "https://www.occ.com.mx/empleos/"
+driver.get(url)
+sleep(5)
+
+#Se extrae el total de anuncios
+Total_anuncios = driver.find_element(By.XPATH, '//p[@class="text-0-2-82 small-0-2-90 midEmphasis-0-2-104"]').text
+print("Total de Anuncios en México: " + (Total_anuncios))
+
+driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+sleep(2)
+
+#Se establece el total de paguinas
+total_paguinas = driver.find_element(By.CSS_SELECTOR, "li.li-0-2-625:nth-child(4)").text
+print("Total de paguinas: " + str(total_paguinas))
+
+#Se establece el total de anuncios en la paguina
+anuncios = driver.find_elements(By.XPATH, '//*[starts-with(@id,"jobcard")]')
+print("Anuncios encontrados: " + str(len(anuncios)))
+sleep(5)
+
+detetctor_linea = 0
+gatillo = 0
+while gatillo < 3:
+    gatillo +=1
+
+    # for anuncio in anuncios:
+    #     try:
+    #         fecha = anuncio.find_element(By.CLASS_NAME, 'date-0-2-567').text
+    #         print('Fecha: ' + str(fecha))
+    #     except:
+    #         pass
+    #     try:
+    #         puesto = anuncio.find_element(By.CLASS_NAME, 'longWord-0-2-575').text
+    #         print('Puesto: ' + str(puesto))
+    #     except Exception as err:
+    #         print(err)
+
+    #     try:
+    #         empresa = anuncio.find_element(By.CLASS_NAME, "fresnel-greaterThanOrEqual-sm").text
+    #         print("EMPRESA: " + str(empresa))
+    #     except:
+    #         print('EMPRESA: ', 'Empresa Confidencial')
+
+    #     try:
+    #         sueldo = anuncio.find_element(By.CLASS_NAME, "salary-0-2-559").text
+    #         print("SUELDO: " + str(sueldo))
+    #     except Exception as err:
+    #         print(err)
+
+
+    #     try:
+    #         ciudad = anuncio.find_element(By.CLASS_NAME, "zonesLinks-0-2-601").text
+    #         print("CIUDAD: " + str(ciudad))
+    #     except:
+    #         pass
+
+    #     try:
+    #         url_busqueda = anuncio.find_element(By.CLASS_NAME, 'jobcard-0-2-558').get_attribute('href')
+    #         print("URL_BUSQUEDA: " + str(url_busqueda))
+    #     except:
+    #         pass
+
+    # Aqui busco la interacción de las paguinas
+    try:
+        boton2 = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, "//ul[@class='pager-0-2-617 desktopPager-0-2-615']/li[last()]/a")))
+        actions = ActionChains(driver)
+        actions.move_to_element(boton2).click().perform()
+    except NoSuchElementException:
+        print("No se encontro el boton2")
+        sleep(5)
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+
+print("Raspado terminado")
+
+'''
+este es el codigo para extraer informacion adentro de los anuncios
+
+despues de la variabel anuncios voy a poner la siguiente linea de codigo
+este bucle for lo tendria que poner despues del cogo para extraer la url del anuncio
+
+links = []
+for link in url_busqueda:
+    comentarios = link.find_element(By.CLASS_NAME, 'la etiqueta de los comentarios').text
+    links.append(comentarios)
+
+print('comentarios: ', comentarios)
+
+'''
+
+
+
+
+
